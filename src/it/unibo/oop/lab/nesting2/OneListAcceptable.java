bba7311e16ab545392fbb252bdebf8a054d0dcf6
Ciprian <ciprianstricescu@gmail.com>
2020-11-15 15:04:00 +0100

    Done ex 5 looking at solutions

---
+++ src/it/unibo/oop/lab/nesting2/OneListAcceptable.java
@@ -0,0 +1,46 @@
+package it.unibo.oop.lab.nesting2;
+
+import java.util.Iterator;
+import java.util.List;
+
+
+public class OneListAcceptable<T> implements Acceptable<T> {
+	
+ private List<T> acceptedList;
+	
+	/**
+	 * @param acceptedList of accepted Element T
+	 */
+	public OneListAcceptable(List<T> acceptedList) {
+		this.acceptedList = acceptedList;
+	}
+	
+	public Acceptor<T> acceptor() {
+		return new Acceptor<T>() {
+			
+			private Iterator<T> actualAcceptedIterator = acceptedList.iterator();
+			
+			public void accept(T newElement) throws ElementNotAcceptedException {
+				if (!actualAcceptedIterator.hasNext() 
+						|| !actualAcceptedIterator.next().equals(newElement)) {
+					throw new ElementNotAcceptedException(newElement);
+				}
+			}
+
+			public void end() throws EndNotAcceptedException {
+				if (actualAcceptedIterator.hasNext()) {
+					throw new EndNotAcceptedException();
+				}
+			}
+			
+		};
+	}
+
+	/**
+	 * @return String rapresentation of acceptedList
+	 */
+	public String toString() {
+		return "OneListAcceptable [acceptedList=" + acceptedList + "]";
+	}
+	
+}
